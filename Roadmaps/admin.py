d9d9c647f23f4aad7580d268ccf399adf41eca67
sunghyun1356 <sunghyun1356@naver.com>
2023-09-14 11:46:15 +0900

    test

--- Roadmaps/admin.py
+++ Roadmaps/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Roadmap)
+admin.site.register(RoadmapDetail)
+admin.site.register(SubjectInRoadmapDetail)
--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Major(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class StudentNumber(models.Model):
+    number = models.IntegerField(default=25)
+
+    def __str__(self):
+        return str(self.number)
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    student_number = models.ForeignKey(StudentNumber, on_delete=models.CASCADE)
+    password = models.CharField(max_length=100)
+    school_email = models.EmailField()
+    primary_major = models.ForeignKey(Major, on_delete=models.CASCADE, related_name='primary_major_users')
+    secondary_major = models.ForeignKey(Major, on_delete=models.CASCADE, related_name='secondary_major_users', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
