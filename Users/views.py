e56625140c7177b9062e1950c4b1b56dd3c4ec28
sunghyun1356 <sunghyun1356@naver.com>
2023-09-19 18:27:38 +0900

    최신화'

--- Users/views.py
+++ Users/views.py
@@ -11,7 +11,7 @@ from django.http import JsonResponse
 
 from rest_framework import status
 from rest_framework import generics
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt import TokenObtainPairView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
