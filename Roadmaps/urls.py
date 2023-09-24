a5ab382dc1e06a2c1273a340e8b4015592ea146b
sunghyun1356 <sunghyun1356@naver.com>
2023-09-24 16:39:21 +0900

    view는 수정완료 (#21)

--- Roadmaps/urls.py
+++ Roadmaps/urls.py
@@ -3,8 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('<str:user_id>/', RoadMapList.as_view()),
-    path('<str:user_id>/1/',RoadMapLectureList.as_view() ),
-    path('<str:user_id>/2/',RoadmapFullView.as_view() )
+
+    path('<str:user_id>/',RoadmapFullView.as_view() )
 
 ]
--- Roadmaps/views.py
+++ Roadmaps/views.py
@@ -6,48 +6,10 @@ from django.http import JsonResponse
 
 # Create your views here.
 
-class RoadMapList(generics.ListAPIView):
-    queryset = Roadmap.objects.all()  
-    serializer_class = RoadMapSerializer
-
-    def get_queryset(self):
-        
-        user_id = self.kwargs['user_id']
-
-        user = MyUser.objects.get(student_number=user_id)
-        
-        return Roadmap.objects.filter(student = user)
-
-class RoadMapLectureList(generics.ListAPIView):
-    queryset = RoadmapDetailLecture.objects.all()  
-    serializer_class = RoadMapDetailLectureSerializer
-
-
-
-class RoadMapDefault(generics.ListCreateAPIView):
-    pass
-
 
 
 # 새로 짠거 # 
 ##########################################
-class RoadmapDetailView(generics.RetrieveAPIView):
-    queryset = Roadmap.objects.all()  
-    serializer_class = RoadMapSerializer
-
-class RoadmapDetailListView(generics.ListAPIView):
-    serializer_class = RoadMapDetailSerializer
-
-    def get_queryset(self):
-        roadmap_id = self.kwargs['roadmap_id']
-        return RoadmapDetail.objects.filter(roadmap=roadmap_id)
-
-class RoadmapDetailLectureListView(generics.ListAPIView):
-    serializer_class = RoadMapDetailLectureSerializer
-
-    def get_queryset(self):
-        roadmap_detail_id = self.kwargs['roadmap_detail_id']
-        return RoadmapDetailLecture.objects.filter(roadmap_detail=roadmap_detail_id)
 
 class RoadmapFullView(generics.ListAPIView):
     queryset = Roadmap.objects.all()
